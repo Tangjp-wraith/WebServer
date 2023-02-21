d3fb2d280a03b6fc4693b12e67b424b392baca1f
Tangjp-wraith <3081129260@qq.com>
2023-02-21 18:22:03 +0800

    move webbench

---
+++ test/buffer_test.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file buffer_test.cpp
+ * @author Tang Jiapeng (tangjiapeng0215@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "gtest/gtest.h"
+#include
---
+++ test/log_test.cpp
@@ -0,0 +1,4 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "../src/include/log.h"
+
